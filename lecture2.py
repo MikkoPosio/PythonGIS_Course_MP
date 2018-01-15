b39023d3d3dd2a5ead85ebb5b5459ff41f0d0ab8
Mikko Posio <mikko.posio@oamk.fi>
2018-01-15 13:51:21 +0200

    Add files via upload

---
+++ lecture2.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 15 11:45:24 2018
+
+Lesson 2 CSC
+
+@author: cscuser
+"""
+
+import geopandas as gpd
+import pandas as pd
+
+# Filepaths
+
+fp = r"C:\Users\cscuser\Desktop\Data\DAMSELFISH_distributions.shp"
+outfp = r"C:\Users\cscuser\Desktop\Data\Selection_DAMSELFISH_distributions.shp"
+#read the data
+data = gpd.read_file(fp)
+# Select attributes and certain rows from the data
+selection = data.loc[data['YEAR']==2012, ['YEAR', 'geometry', 'BINOMIAL']] 
+selection_all = data.loc[data['YEAR']==2012] 
+#write data to file
+selection.to_file(outfp)
+
+#create a neew column call area of the Polygons
+data['area_decimal_degrees'] = data.area
+data['centroid'] = data.centroid
+
+#centroids
+
+centroids = data[['centroid', 'BINOMIAL', 'YEAR']]
+
+centroids = centroids.rename(columns={'centroid': 'geometry'})
+
+#converts the pandas to GeaDataFrame
+centroids = gpd.GeoDataFrame(centroids, geometry='geometry', crs=data.crs)
+
