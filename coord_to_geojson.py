7744a1861027edd898d27888175c8f8af9c10a74
Mikko Posio <mikko.posio@oamk.fi>
2018-01-15 16:06:53 +0200

    Add files via upload

--- coord_to_geojson.py
+++ coord_to_geojson.py
@@ -7,12 +7,12 @@ Created on Mon Jan 15 15:20:05 2018
 
 import pandas as pd
 import geopandas as gpd
-from shapely.geometry import Point
+from shapely.geometry import Point, LineString
 
 #filepath
 
 txt_fp = r"C:\Users\cscuser\Desktop\Data\travelTimes_2015_Helsinki.txt"
-
+#excel_fp = r"C:\Users\cscuser\Desktop\Data\travelTimes_2015_Helsinki.xlsx"
 #stream = open(txt_fp, 'r')
 #stream.readline()
 #stream.close() #control + 1 change lines to #dfgd
@@ -20,3 +20,13 @@ txt_fp = r"C:\Users\cscuser\Desktop\Data\travelTimes_2015_Helsinki.txt"
 #read the data
 
 data = pd.read_csv(txt_fp, sep=';')
+#data = pd.read_excel(excel_fp, sheet_name="travelTimes_2015_Helsinki")
+data['origin_point'] = data.apply(lambda row: Point(row['from_x'], row['from_y']), axis=1)
+data['dest_point'] = data.apply(lambda row: Point(row['to_x'], row['to_y']), axis=1)
+
+# create a GeoDataFrame
+geo = gpd.GeoDataFrame(data, geometry='dest_point', crs={'init':'epsg4326'})
+#create LineString between points
+geo['line'] = geo.apply(lambda row: LineString([row['origin_point'], row['dest_point']]), axis=1)
+geo = geo.set_geometry('line')
+geo[0:10].plot()
